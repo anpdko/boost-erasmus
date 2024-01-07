9034dfdee9d88a93cca3ca94be4585597198e01a
anpdko <anpdko@gmail.com>
2024-01-07 04:53:41 +0200

    fix deploy

--- src/pages/Contacts/Contacts.tsx
+++ src/pages/Contacts/Contacts.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
+// import React from 'react'
 import s from './Contacts.module.scss'
-import contacts from '../../data/contacts';
+// import contacts from '../../data/contacts';
 
 const Contacts = () => {
    return (
-      <div>
+      <div className={s.contacts}>
          
       </div>
    );
